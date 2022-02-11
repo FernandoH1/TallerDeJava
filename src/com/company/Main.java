1b6eedd1c56b34603d0ee082cf5b33230f12f083
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-02-11 12:12:02 -0300

    EJercicio 16 -completado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,61 +7,62 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-       Ejercicio15();
+        Ejercicio16();
+
     }
 
-    public static void Ejercicio1(int numero1,int numero2){
-        if(numero1 == numero2){
+    public static void Ejercicio1(int numero1, int numero2) {
+        if (numero1 == numero2) {
             System.out.println("Los Números son IGUALES!!");
-        }else{
-            System.out.println("El número "+Math.max(numero1,numero2)+" es mayor que el número "+Math.min(numero1,numero2));
+        } else {
+            System.out.println("El número " + Math.max(numero1, numero2) + " es mayor que el número " + Math.min(numero1, numero2));
         }
     }
 
-    public static void Ejercicio2(){
+    public static void Ejercicio2() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un Número: ");
         int num1 = sc.nextInt();
         System.out.println("Ingrese el Número 2: ");
         int num2 = sc.nextInt();
-        Ejercicio1(num1,num2);
+        Ejercicio1(num1, num2);
     }
 
-    public static void Ejercicio3(){
+    public static void Ejercicio3() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el radio: ");
         double radio = sc.nextDouble();
-        double calcularElArea = Math.PI*(Math.pow(radio,2));
-        System.out.println("El área del Círculo es: "+calcularElArea);
+        double calcularElArea = Math.PI * (Math.pow(radio, 2));
+        System.out.println("El área del Círculo es: " + calcularElArea);
     }
 
-    public static void Ejercicio4(){
+    public static void Ejercicio4() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese el precio del Producto: ");
         double precio = sc.nextDouble();
-        double precioFinal = (precio*0.21);
-        precioFinal = precioFinal+precio;
-        System.out.println("El precio Final con IVA es: "+precioFinal);
+        double precioFinal = (precio * 0.21);
+        precioFinal = precioFinal + precio;
+        System.out.println("El precio Final con IVA es: " + precioFinal);
     }
 
-    public static void Ejercicio5(){
-        int i=1;
-        while(i<101){
-            if(i%2 == 0){
-                System.out.println(i+" - Es Par!");
-            }else{
-                System.out.println(i+" - Es Impar!");
+    public static void Ejercicio5() {
+        int i = 1;
+        while (i < 101) {
+            if (i % 2 == 0) {
+                System.out.println(i + " - Es Par!");
+            } else {
+                System.out.println(i + " - Es Impar!");
             }
             i++;
         }
     }
 
-    public static void Ejercicio6(){
+    public static void Ejercicio6() {
         for (int i = 1; i < 101; i++) {
-            if( i % 2 == 0){
-                System.out.println(i+" - Es Par!");
-            }else{
-                System.out.println(i+" - Es Impar!");
+            if (i % 2 == 0) {
+                System.out.println(i + " - Es Par!");
+            } else {
+                System.out.println(i + " - Es Impar!");
             }
         }
     }
@@ -70,17 +71,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un Número: ");
         int numero = sc.nextInt();
-            if (numero == 0) {
-                System.out.println(numero + " Es IGUAL que 0");
-            } else if(numero > 0){
-                System.out.println(numero + " Es MAYOR que 0");
-            }else{
-                System.out.println("Por Favor ingrese un Número (Mayor) o (Igual) a 0");
-                Ejercicio7();
-            }
+        if (numero == 0) {
+            System.out.println(numero + " Es IGUAL que 0");
+        } else if (numero > 0) {
+            System.out.println(numero + " Es MAYOR que 0");
+        } else {
+            System.out.println("Por Favor ingrese un Número (Mayor) o (Igual) a 0");
+            Ejercicio7();
+        }
     }
 
-    public static void Ejercicio8(){
+    public static void Ejercicio8() {
         menuEjercicio8();
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un dia de la Semanda(número): ");
@@ -112,82 +113,82 @@ public class Main {
         }
     }
 
-    public static void menuEjercicio8(){
+    public static void menuEjercicio8() {
         System.out.println("//---------------- Menú ----------------//\n" +
-        "| 1 - Lunes                              |\n" +
-        "| 2 - Martes                             |\n" +
-        "| 3 - Miércoles                          |\n" +
-        "| 4 - Jueves                             |\n" +
-        "| 5 - Viernes                            |\n" +
-        "| 6 - Sábado                             |\n" +
-        "| 7 - Domingo                            |\n" +
-        "//---------------------------------------//");
+                "| 1 - Lunes                              |\n" +
+                "| 2 - Martes                             |\n" +
+                "| 3 - Miércoles                          |\n" +
+                "| 4 - Jueves                             |\n" +
+                "| 5 - Viernes                            |\n" +
+                "| 6 - Sábado                             |\n" +
+                "| 7 - Domingo                            |\n" +
+                "//---------------------------------------//");
     }
 
 
-
-    public static void Ejercicio9(){
+    public static void Ejercicio9() {
         String frase = "La sonrisa sera la mejor arma contra la tristeza";
-        String remplazoFrase = frase.replace("a","e");
-        System.out.println("FRASE ANTERIOR REMPLAZADA: "+remplazoFrase);
+        String remplazoFrase = frase.replace("a", "e");
+        System.out.println("FRASE ANTERIOR REMPLAZADA: " + remplazoFrase);
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese otra frase para unir a la anterior: ");
         String frase2 = sc.nextLine();
-        System.out.println(remplazoFrase+" "+frase2);
+        System.out.println(remplazoFrase + " " + frase2);
     }
 
-    public static void Ejercicio10(){
+    public static void Ejercicio10() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese una frase: ");
         String frase = sc.nextLine();
-        String fraseSinEspacios = frase.replace(" ","");
+        String fraseSinEspacios = frase.replace(" ", "");
         System.out.println(fraseSinEspacios);
     }
-     public static void Ejercicio11(){
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Ingrese una frase: ");
-         String frase = sc.nextLine();
-         int contadorVocales = 0;
-         for (int i = 0; i < frase.length() ; i++) {
-             if ((frase.charAt(i)=='a') || (frase.charAt(i)=='e') || (frase.charAt(i)=='i') || (frase.charAt(i)=='o') || (frase.charAt(i)=='u')){
-                 contadorVocales++;
-             }
-         }
-         System.out.println("EL largo de la FRASE(con espacios) es: "+frase.length()+" y el numero VOCALES que contiene son: "+contadorVocales);
-     }
 
-     public static void Ejercicio12(){
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Ingrese una palabra: ");
-         String palabra = sc.nextLine();
-         System.out.println("Ingrese otra palabra: ");
-         String palabra2 = sc.nextLine();
-         if(palabra.equals(palabra2)){
-             System.out.println("Son Iguales");
-         }else
-             System.out.println("Son Distintas");
-     }
+    public static void Ejercicio11() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una frase: ");
+        String frase = sc.nextLine();
+        int contadorVocales = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            if ((frase.charAt(i) == 'a') || (frase.charAt(i) == 'e') || (frase.charAt(i) == 'i') || (frase.charAt(i) == 'o') || (frase.charAt(i) == 'u')) {
+                contadorVocales++;
+            }
+        }
+        System.out.println("EL largo de la FRASE(con espacios) es: " + frase.length() + " y el numero VOCALES que contiene son: " + contadorVocales);
+    }
 
-     public static void Ejercicio13(){
-         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-         System.out.println("(AAAA/MM/DD) (HH:MM:SS) -> "+dtf.format(LocalDateTime.now()));
-     }
+    public static void Ejercicio12() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una palabra: ");
+        String palabra = sc.nextLine();
+        System.out.println("Ingrese otra palabra: ");
+        String palabra2 = sc.nextLine();
+        if (palabra.equals(palabra2)) {
+            System.out.println("Son Iguales");
+        } else
+            System.out.println("Son Distintas");
+    }
 
-    public static void Ejercicio14(){
+    public static void Ejercicio13() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println("(AAAA/MM/DD) (HH:MM:SS) -> " + dtf.format(LocalDateTime.now()));
+    }
+
+    public static void Ejercicio14() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese un Número: ");
         int numero = sc.nextInt();
-        while(numero < 1001){
+        while (numero < 1001) {
             System.out.println(numero);
-            numero = numero+=2;
+            numero = numero += 2;
         }
     }
 
-    public static void Ejercicio15(){
+    public static void Ejercicio15() {
         Scanner sc = new Scanner(System.in);
         boolean salir = false;
         menuEjercicio15();
-        do{
+        do {
             System.out.println("Ingrese un Número: ");
             int numero = sc.nextInt();
             switch (numero) {
@@ -219,10 +220,10 @@ public class Main {
                     break;
             }
 
-        }while(!salir);
+        } while (!salir);
     }
 
-    public static void menuEjercicio15(){
+    public static void menuEjercicio15() {
         System.out.println("//-------------GESTION CINEMATOGRÁFICA ------------//\n" +
                 "| 1-NUEVO ACTOR                                    |\n" +
                 "| 2-BUSCAR ACTOR                                   |\n" +
@@ -233,5 +234,41 @@ public class Main {
                 "| 7-VER CATEGORIA DE LAS PELICULAS DE LOS ACTORES  |\n" +
                 "| 8-SALIR                                          |\n" +
                 "//------------------------------------------------//");
+    }
+
+    public static void Ejercicio16(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nombre de la Persona: ");
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese sexo de la Persona(H o M): ");
+        char sexo = sc.next().charAt(0);
+        System.out.println("Ingrese edad de la Persona: ");
+        int edad = sc.nextInt();
+        System.out.println("Ingrese peso de la Persona: ");
+        int peso = sc.nextInt();
+        System.out.println("Ingrese altura(en metros) de la Persona: Ejemplo (1,79): ");
+        double altura = sc.nextDouble();
+
+
+        Persona persona = new Persona(nombre,edad,sexo,peso,altura);
+        Persona persona2 = new Persona(nombre,edad,sexo);
+        Persona persona3 = new Persona();
+        persona3.setNombre("Maria");
+        persona3.setEdad(12);
+        persona3.setSexo('M');
+        persona3.setPeso(70);
+        persona3.setAltura(1.83);
+
+        System.out.printf(persona.getNombre()+" ");persona.calcularIMC();
+        System.out.printf(persona2.getNombre()+" ");persona2.calcularIMC();
+        System.out.printf(persona3.getNombre()+" ");persona3.calcularIMC();
+
+        System.out.printf(persona.getNombre()+" ");persona.esMayorDeEdad();
+        System.out.printf(persona2.getNombre()+" "); persona2.esMayorDeEdad();
+        System.out.printf(persona3.getNombre()+" ");persona3.esMayorDeEdad();
+
+        System.out.println(persona.toString());
+        System.out.println(persona2.toString());
+        System.out.println(persona3.toString());
     }
 }
--- src/com/company/Persona.java
+++ src/com/company/Persona.java
@@ -1,5 +1,116 @@
 package com.company;
 
 public class Persona {
+    private String nombre = null;
+    private int edad = 0;
+    private char sexo = ' ';
+    private int peso = 0;
+    private double altura = 0;
+    private int dni = 0;
+
+    public Persona() {
+        this.dni = generarDNI();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public Persona(String nombre, int edad, char sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = comprobarSexo(sexo);
+        this.dni = generarDNI();
+    }
+
+    public Persona(String nombre, int edad, char sexo, int peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = comprobarSexo(sexo);
+        this.peso = peso;
+        this.altura = altura;
+        this.dni = generarDNI();
+    }
+
+    public int calcularIMC() {
+        int imc = 2;
+        if (this.peso < 20) {
+            System.out.println("Esta por el peso por debajo de lo normal");
+            return -1;
+        } else if (this.peso < 26 && this.peso > 19) {
+            System.out.println("Su peso es Normal");
+            return 0;
+        } else if (this.peso > 25) {
+            System.out.println("Esta por encima de su peso normal(Sobrepeso)");
+            return 1;
+        }
+        return imc;
+    }
+
+    public boolean esMayorDeEdad() {
+        if (this.edad >= 18) {
+            System.out.println("Es Mayor");
+            return true;
+        } else {
+            System.out.println("Es Menor");
+            return false;
+        }
+    }
+
+    private char comprobarSexo(char sexo) {
+        if (sexo == 'M' || sexo == 'm') {
+            return 'M';
+        } else if (sexo == 'H' || sexo == 'h') {
+            return 'H';
+        } else {
+            return 'H';
+        }
+    }
+
+    public String toString() {
+        return "Nombre: " + this.getNombre() + " - Edad: " + String.valueOf(this.edad) + " - Sexo: " + this.sexo + " - Peso: " + String.valueOf(this.peso) + "kg - Altura: " + String.valueOf(this.altura) + "m";
+    }
+
+    private int generarDNI() {
+        double dni = 10000000 + Math.random() * 90000000;
+        return (int) dni;
+    }
+
 
 }
